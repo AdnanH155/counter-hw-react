4f6ae3462f3f547849d43b329fd8ea351c086047
Adnan Hussain <adnan@adnans-air-2.home>
2023-10-23 20:12:08 +0100

    counter update

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Main from "./Components/Main";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
 import Link from "./Components/Link";
+import Nav from "./Components/Nav";
 class App extends Component {
   state = { count: 1 };
 
@@ -12,17 +13,25 @@ class App extends Component {
   minus = () => {
     this.setState({ count: this.state.count - 1 });
   };
-
+  divide = () => {
+    this.setState({ count: this.state.count / 2 });
+  };
+  multiple = () => {
+    this.setState({ count: this.state.count * 4 });
+  };
   render() {
     return (
       <div>
-        <button onClick={this.add}>+</button>
-        <p>{this.state.count}</p>
-        <button onClick={this.minus}>-</button>
         <Header />
+        <Nav />
         <Main />
         <Footer />
         <Link />
+        <p>{this.state.count}</p>
+        <button onClick={this.add}>+</button>
+        <button onClick={this.minus}>-</button>
+        <button onClick={this.multiple}>X 4</button>
+        <button onClick={this.divide}>/ 2</button>
       </div>
     );
   }
